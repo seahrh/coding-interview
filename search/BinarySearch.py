09673bfec10151b64185f35e0a4a78b47f2425a9
Ruhong <seahrh@gmail.com>
2019-05-14 18:02:21 +0800

    binary search array with empty strings

--- search/BinarySearch.py
+++ search/BinarySearch.py
@@ -4,8 +4,34 @@ def search(arr, key):
     begin = 0
     end = len(arr) - 1
     while begin <= end:
-        mid = int((end - begin) / 2 + begin)
-        print('begin={}, end={}, mid={}'.format(begin, end, mid))
+        mid = int((end - begin) / 2 + begin)  # avoid integer overflow
+        if arr[mid] == key:
+            return mid
+        if key > arr[mid]:
+            begin = mid + 1
+            continue
+        if key < arr[mid]:
+            end = mid - 1
+    return -1
+
+
+def search_array_with_empty_strings(arr, key):
+    # Search a sorted array of strings which is interspersed with empty strings.
+    # [CTCI Q9.5]
+    if len(arr) == 0:
+        raise ValueError('arr must not be empty')
+    begin = 0
+    end = len(arr) - 1
+    while begin <= end:
+        # ensure there is something at the end
+        while begin <= end and arr[end] == '':
+            end -= 1
+        if end < begin:
+            return -1
+        mid = int((end - begin) / 2 + begin)  # avoid integer overflow
+        # Keep going right until a non-empty string is found
+        while arr[mid] == '':
+            mid += 1
         if arr[mid] == key:
             return mid
         if key > arr[mid]:
--- search/tests/test_binary_search.py
+++ search/tests/test_binary_search.py
@@ -25,5 +25,36 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(search(arr=[1, 2, 4], key=3), -1)
 
 
+class TestBinarySearchArrayWithEmptyStrings(unittest.TestCase):
+    def test_search_array_of_length_one(self):
+        self.assertEqual(search_array_with_empty_strings(arr=['b'], key='b'), 0)
+        self.assertEqual(search_array_with_empty_strings(arr=['b'], key='a'), -1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b'], key='c'), -1)
+
+    def test_search_array_of_length_two(self):
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c'], key='b'), 0)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c'], key='c'), 1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', ''], key='b'), 0)
+        self.assertEqual(search_array_with_empty_strings(arr=['', 'c'], key='c'), 1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c'], key='a'), -1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c'], key='d'), -1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'd'], key='c'), -1)
+
+    def test_search_array_of_length_three(self):
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c', 'd'], key='b'), 0)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c', 'd'], key='c'), 1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c', 'd'], key='d'), 2)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', '', 'd'], key='b'), 0)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c', ''], key='c'), 1)
+        self.assertEqual(search_array_with_empty_strings(arr=['', 'c', 'd'], key='d'), 2)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c', 'd'], key='a'), -1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'c', 'd'], key='e'), -1)
+        self.assertEqual(search_array_with_empty_strings(arr=['b', 'd', 'e'], key='c'), -1)
+
+    def test_when_array_has_only_empty_strings_then_not_found(self):
+        self.assertEqual(search_array_with_empty_strings(arr=[''], key='a'), -1)
+        self.assertEqual(search_array_with_empty_strings(arr=['', ''], key='a'), -1)
+
+
 if __name__ == '__main__':
     unittest.main()
