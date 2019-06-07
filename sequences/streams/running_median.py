10881dcd0f3144b98c97002d4a5efc23bbf40641
Ruhong <seahrh@gmail.com>
2019-06-07 15:23:44 +0800

    get running median with 2 heaps

--- sequences/streams/running_median.py
+++ sequences/streams/running_median.py
@@ -6,11 +6,39 @@ import heapq
 
 
 class RunningMedian:
+    """Use two priority heaps: a max heap for the values below the median,
+    and a min heap for the values above the median.
+
+    When a new value arrives,
+    it is placed in the maxHeap if the value is less than or equal to the median,
+    otherwise it is placed into the minHeap.
+    The heap sizes can be equal, or the maxHeap may have one extra element.
+    This constraint can easily be restored by shifting an element from one heap to the other.
+    The median takes O(1) time, by looking at the top element(s).
+    Updates take O(lg n) time.
+    The heaps take O(n) space.
+
+    Max heap is implemented in Python by multiplying the value by -1
+    before adding it to the heap ("taking the inverse").
+    """
+
     min_heap = []
     max_heap = []
 
     def add(self, i):
-        pass
+        """Maintains invariant that len(max_heap) >= len(min_heap)"""
+        if len(self.max_heap) == len(self.min_heap):
+            if len(self.min_heap) and i > self.min_heap[0]:
+                j = heapq.heappushpop(self.min_heap, i)
+                heapq.heappush(self.max_heap, j * -1)
+            else:
+                heapq.heappush(self.max_heap, i * -1)
+        else:
+            if i < self.max_heap[0]:
+                j = heapq.heappushpop(self.max_heap, i * -1) * -1
+                heapq.heappush(self.min_heap, j)
+            else:
+                heapq.heappush(self.min_heap, i)
 
     def median(self):
         # maxHeap is always at least as big as minHeap.
