94b9dfe140c501c51d5de6728459c9bf292f2533
Ruhong <seahrh@gmail.com>
2019-06-10 16:28:16 +0800

    binary search 2d sorted matrix

--- search/sorted_matrix_search.py
+++ search/sorted_matrix_search.py
@@ -3,6 +3,7 @@ Sorted Matrix Search: Given an M x N matrix in which each row and each column is
 ascending order, write a method to find an element.
 (10.9, p421)
 """
+from collections import namedtuple
 
 
 def find(matrix, key):
@@ -32,3 +33,81 @@ def find(matrix, key):
         else:
             i += 1
     return -1
+
+
+Cell = namedtuple('Cell', 'row col')
+
+
+def _in_bounds(matrix, cell):
+    return 0 <= cell.row < len(matrix) and 0 <= cell.col < len(matrix[0])
+
+
+def _is_before(left, right):
+    return left.row <= right.row and left.col <= right.col
+
+
+def _average(min_cell, max_cell):
+    return Cell(
+        row=int((min_cell.row / 2) + (max_cell.row / 2)),
+        col=int((min_cell.col / 2) + (max_cell.col / 2)),
+    )
+
+
+def _partition_and_search(matrix, origin, dest, key, pivot):
+    lower_left_origin = Cell(row=pivot.row, col=origin.col)
+    lower_left_dest = Cell(row=dest.row, col=pivot.col - 1)
+    upper_right_origin = Cell(row=origin.row, col=pivot.col)
+    upper_right_dest = Cell(row=pivot.row - 1, col=dest.col)
+    res = _binary_search(matrix, lower_left_origin, lower_left_dest, key)
+    if res is None:
+        res = _binary_search(matrix, upper_right_origin, upper_right_dest, key)
+    return res
+
+
+def _binary_search(matrix, origin, dest, key):
+    """Partition the matrix into 4 quadrants and
+    recursively search the lower left and upper right quadrants.
+    These too will get divided further into quadrants and searched.
+    Idea: In a quadrant, the origin is the smallest value and destination is largest.
+    e.g.
+    [min, ---, ---]
+    [---, ---, ---]
+    [---, ---, max]
+    The key cannot be in the upper left quadrant because key > quadrant destination.
+    The key cannot be in the lower right quadrant because key < quadrant origin.
+    Since the diagonal is sorted, we can use binary search.
+    """
+    if not _in_bounds(matrix, origin):
+        return None
+    if not _in_bounds(matrix, dest):
+        return None
+    if not _is_before(origin, dest):
+        return None
+    if matrix[origin.row][origin.col] == key:
+        return origin
+    # set `lo` and `hi` to be the start and end of the diagonal respectively.
+    # since matrix may not be square, end of the diagonal may not equal `hi`
+    diagonal_dist = min(dest.row - origin.row, dest.col - origin.col)
+    lo = origin
+    hi = Cell(row=lo.row + diagonal_dist, col=lo.col + diagonal_dist)
+    # binary search on the diagonal, looking for the 1st element > key
+    while _is_before(lo, hi):
+        mid = _average(lo, hi)
+        if key == matrix[mid.row][mid.col]:
+            return mid
+        if key > matrix[mid.row][mid.col]:
+            lo = Cell(row=mid.row + 1, col=mid.col + 1)
+        else:
+            hi = Cell(row=mid.row - 1, col=mid.col - 1)
+    return _partition_and_search(matrix, origin, dest, key, pivot=lo)
+
+
+def binary_search(matrix, key):
+    if len(matrix) == 0:
+        raise ValueError('matrix must not be empty')
+    return _binary_search(
+        matrix=matrix,
+        origin=Cell(0, 0),
+        dest=Cell(len(matrix) - 1, len(matrix[0]) - 1),
+        key=key
+    )
--- search/tests/test_sorted_matrix_search.py
+++ search/tests/test_sorted_matrix_search.py
@@ -11,34 +11,113 @@ class TestSortedMatrixSearch(unittest.TestCase):
         self.assertEqual(find([[1]], -1), -1)
 
     def test_when_matrix_is_2x2(self):
+        self.assertEqual(find([[1, 2], [3, 4]], 1), (0, 0))
+        self.assertEqual(find([[1, 2], [3, 4]], 2), (0, 1))
         self.assertEqual(find([[1, 2], [3, 4]], 3), (1, 0))
+        self.assertEqual(find([[1, 2], [3, 4]], 4), (1, 1))
         self.assertEqual(find([[1, 2], [3, 4]], 30), -1)
 
     def test_when_matrix_is_3x3(self):
-        self.assertEqual(find([
-            [1, 2, 30],
-            [2, 3, 40],
-            [30, 40, 50]
-        ], 3), (1, 1))
-        self.assertEqual(find([
-            [1, 2, 30],
-            [2, 3, 40],
-            [30, 40, 50]
-        ], 31), -1)
+        matrix = [
+            [10, 20, 30],
+            [40, 50, 60],
+            [70, 80, 90]
+        ]
+        self.assertEqual(find(matrix, 10), (0, 0))
+        self.assertEqual(find(matrix, 20), (0, 1))
+        self.assertEqual(find(matrix, 30), (0, 2))
+        self.assertEqual(find(matrix, 40), (1, 0))
+        self.assertEqual(find(matrix, 50), (1, 1))
+        self.assertEqual(find(matrix, 60), (1, 2))
+        self.assertEqual(find(matrix, 70), (2, 0))
+        self.assertEqual(find(matrix, 80), (2, 1))
+        self.assertEqual(find(matrix, 90), (2, 2))
+        self.assertEqual(find(matrix, 51), -1)
 
     def test_when_matrix_is_4x4(self):
-        self.assertEqual(find([
-            [1, 2, 30, 40],
-            [2, 3, 40, 50],
-            [30, 40, 50, 60],
-            [40, 50, 60, 70]
-        ], 3), (1, 1))
-        self.assertEqual(find([
-            [1, 2, 30, 40],
-            [2, 3, 40, 50],
-            [30, 40, 50, 60],
-            [40, 50, 60, 70]
-        ], 31), -1)
+        matrix = [
+            [10, 20, 30, 40],
+            [50, 60, 70, 80],
+            [90, 100, 110, 120],
+            [130, 140, 150, 160]
+        ]
+        self.assertEqual(find(matrix, 10), (0, 0))
+        self.assertEqual(find(matrix, 20), (0, 1))
+        self.assertEqual(find(matrix, 30), (0, 2))
+        self.assertEqual(find(matrix, 40), (0, 3))
+        self.assertEqual(find(matrix, 50), (1, 0))
+        self.assertEqual(find(matrix, 60), (1, 1))
+        self.assertEqual(find(matrix, 70), (1, 2))
+        self.assertEqual(find(matrix, 80), (1, 3))
+        self.assertEqual(find(matrix, 90), (2, 0))
+        self.assertEqual(find(matrix, 100), (2, 1))
+        self.assertEqual(find(matrix, 110), (2, 2))
+        self.assertEqual(find(matrix, 120), (2, 3))
+        self.assertEqual(find(matrix, 130), (3, 0))
+        self.assertEqual(find(matrix, 140), (3, 1))
+        self.assertEqual(find(matrix, 150), (3, 2))
+        self.assertEqual(find(matrix, 160), (3, 3))
+        self.assertEqual(find(matrix, 51), -1)
+
+
+class TestSortedMatrixBinarySearch(unittest.TestCase):
+
+    def test_when_matrix_is_empty_then_raise_error(self):
+        self.assertRaises(ValueError, binary_search, [], 1)
+
+    def test_when_matrix_is_1x1(self):
+        self.assertEqual(binary_search([[1]], 1), Cell(0, 0))
+        self.assertEqual(binary_search([[1]], -1), None)
+
+    def test_when_matrix_is_2x2(self):
+        matrix = [[10, 20], [30, 40]]
+        self.assertEqual(binary_search(matrix, 10), Cell(0, 0))
+        self.assertEqual(binary_search(matrix, 20), Cell(0, 1))
+        self.assertEqual(binary_search(matrix, 30), Cell(1, 0))
+        self.assertEqual(binary_search(matrix, 40), Cell(1, 1))
+        self.assertEqual(binary_search(matrix, 11), None)
+
+    def test_when_matrix_is_3x3(self):
+        matrix = [
+            [10, 20, 30],
+            [40, 50, 60],
+            [70, 80, 90]
+        ]
+        self.assertEqual(binary_search(matrix, 10), Cell(0, 0))
+        self.assertEqual(binary_search(matrix, 20), Cell(0, 1))
+        self.assertEqual(binary_search(matrix, 30), Cell(0, 2))
+        self.assertEqual(binary_search(matrix, 40), Cell(1, 0))
+        self.assertEqual(binary_search(matrix, 50), Cell(1, 1))
+        self.assertEqual(binary_search(matrix, 60), Cell(1, 2))
+        self.assertEqual(binary_search(matrix, 70), Cell(2, 0))
+        self.assertEqual(binary_search(matrix, 80), Cell(2, 1))
+        self.assertEqual(binary_search(matrix, 90), Cell(2, 2))
+        self.assertEqual(binary_search(matrix, 51), None)
+
+    def test_when_matrix_is_4x4(self):
+        matrix = [
+            [10, 20, 30, 40],
+            [50, 60, 70, 80],
+            [90, 100, 110, 120],
+            [130, 140, 150, 160]
+        ]
+        self.assertEqual(binary_search(matrix, 10), Cell(0, 0))
+        self.assertEqual(binary_search(matrix, 20), Cell(0, 1))
+        self.assertEqual(binary_search(matrix, 30), Cell(0, 2))
+        self.assertEqual(binary_search(matrix, 40), Cell(0, 3))
+        self.assertEqual(binary_search(matrix, 50), Cell(1, 0))
+        self.assertEqual(binary_search(matrix, 60), Cell(1, 1))
+        self.assertEqual(binary_search(matrix, 70), Cell(1, 2))
+        self.assertEqual(binary_search(matrix, 80), Cell(1, 3))
+        self.assertEqual(binary_search(matrix, 90), Cell(2, 0))
+        self.assertEqual(binary_search(matrix, 100), Cell(2, 1))
+        self.assertEqual(binary_search(matrix, 110), Cell(2, 2))
+        self.assertEqual(binary_search(matrix, 120), Cell(2, 3))
+        self.assertEqual(binary_search(matrix, 130), Cell(3, 0))
+        self.assertEqual(binary_search(matrix, 140), Cell(3, 1))
+        self.assertEqual(binary_search(matrix, 150), Cell(3, 2))
+        self.assertEqual(binary_search(matrix, 160), Cell(3, 3))
+        self.assertEqual(binary_search(matrix, 51), None)
 
 
 if __name__ == '__main__':
