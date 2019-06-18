df71a16dc7b23f5e69947032926679e3ffbd4fec
Ruhong <seahrh@gmail.com>
2019-06-19 03:35:24 +0800

    shuffle recursively

---
+++ recursion/shuffle.py
@@ -0,0 +1,26 @@
+"""
+Shuffle: Write a method to shuffle a deck of cards. It must be a perfect shuffle-in other words, each
+of the 52! permutations of the deck has to be equally likely. Assume that you are given a random
+number generator which is perfect.
+
+(17.2, p544)
+"""
+from random import randint
+
+
+def shuffle_rec(cards, index):
+    """Base case and build approach:
+    First shuffle the first n - 1 elements. Then, we would take the nth
+    element and randomly swap it with an element in the array.
+    O(n) time and O(n) space.
+    """
+    if index < 0:
+        raise ValueError('index must not be negative number')
+    if index == 0:
+        return cards
+    shuffle_rec(cards, index - 1)
+    k = randint(0, index)
+    tmp = cards[k]
+    cards[k] = cards[index]
+    cards[index] = tmp
+    return cards
---
+++ recursion/tests/test_shuffle.py
@@ -0,0 +1,32 @@
+import unittest
+from recursion.shuffle import *
+
+
+class TestShuffleRecursive(unittest.TestCase):
+    def test_given_empty_array_then_return_same_array(self):
+        self.assertListEqual(shuffle_rec(cards=[], index=0), [])
+
+    def test_given_array_of_length_one_then_return_same_array(self):
+        self.assertListEqual(shuffle_rec(cards=[1], index=0), [1])
+
+    def test_given_negative_index_number_then_raise_error(self):
+        self.assertRaises(ValueError, shuffle_rec, [1, 2], -1)
+
+    def test_given_array_of_length_two(self):
+        permutations = set()
+        for _ in range(10):
+            permutations.add(tuple(shuffle_rec(cards=[1, 2], index=1)))
+        self.assertEqual(len(permutations), 2)
+        self.assertSetEqual(permutations, {(1, 2), (2, 1)})
+
+    def test_given_array_of_length_three(self):
+        permutations = set()
+        for _ in range(40):
+            permutations.add(tuple(shuffle_rec(cards=[1, 2, 3], index=2)))
+        self.assertEqual(len(permutations), 6)
+        self.assertSetEqual(permutations, {
+            (1, 2, 3), (3, 2, 1), (1, 3, 2), (2, 3, 1), (3, 1, 2), (2, 1, 3)})
+
+
+if __name__ == '__main__':
+    unittest.main()
