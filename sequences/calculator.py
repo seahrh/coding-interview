0b25b36b4d54facf4d5409602458eb3d5112a333
Ruhong <seahrh@gmail.com>
2019-06-18 17:43:48 +0800

    fix bug: parse divide operator

--- sequences/calculator.py
+++ sequences/calculator.py
@@ -6,37 +6,50 @@ Input: 2*3+5/6*3+15
 Output: 23.5
 
 (16.26, p535)
+Solution with two stacks (1 stack for operators and 1 stack for operands)
+takes O(n) time and O(n) space, where n is the length of the expression to evaluate.
 """
 from enum import Enum
 
 
 class Operator(Enum):
-    ADD = 1
-    SUBTRACT = 1
-    MULTIPLY = 2
-    DIVIDE = 2
-    NOOP = 0
+    ADD = '+'
+    SUBTRACT = '-'
+    MULTIPLY = '*'
+    DIVIDE = '/'
+    NOOP = ''
 
     @staticmethod
     def parse(string):
-        if string == '+':
+        if string == Operator.ADD.value:
             return Operator.ADD
-        if string == '-':
+        if string == Operator.SUBTRACT.value:
             return Operator.SUBTRACT
-        if string == '*':
+        if string == Operator.MULTIPLY.value:
             return Operator.MULTIPLY
-        if string == '/':
+        if string == Operator.DIVIDE.value:
             return Operator.DIVIDE
         return None
 
+    def priority(self):
+        if self == Operator.ADD:
+            return 1
+        if self == Operator.SUBTRACT:
+            return 1
+        if self == Operator.MULTIPLY:
+            return 2
+        if self == Operator.DIVIDE:
+            return 2
+        return 0
+
     def apply(self, left, right):
-        if self.name == Operator.ADD:
+        if self == Operator.ADD:
             return left + right
-        if self.name == Operator.SUBTRACT:
+        if self == Operator.SUBTRACT:
             return left - right
-        if self.name == Operator.MULTIPLY:
+        if self == Operator.MULTIPLY:
             return left * right
-        if self.name == Operator.DIVIDE:
+        if self == Operator.DIVIDE:
             return left / right
         return right
 
@@ -47,7 +60,7 @@ def _collapse(future_top, operand_stack, operator_stack):
     apply the operator popped from the top of the operator stack,
     and then push that onto the numbers stack."""
     while len(operator_stack) >= 1 and len(operand_stack) >= 2:
-        if future_top.value > operator_stack[-1].value:
+        if future_top.priority() > operator_stack[-1].priority():
             break
         right = operand_stack.pop()
         left = operand_stack.pop()
@@ -56,6 +69,8 @@ def _collapse(future_top, operand_stack, operator_stack):
 
 
 def calculate(string):
+    if len(string.strip()) < 6:
+        raise ValueError('Expression must have at least one operator and two operands')
     operand_stack = []
     operator_stack = []
     for term in string.split():
--- sequences/tests/test_calculator.py
+++ sequences/tests/test_calculator.py
@@ -2,12 +2,18 @@ import unittest
 from sequences.calculator import *
 
 
-@unittest.skip
 class TestCalculator(unittest.TestCase):
-    def test_given_example(self):
+    def test_given_expression_too_short_then_raise_error(self):
+        self.assertRaises(ValueError, calculate, '2 *         ')
+
+    def test_given_example_1(self):
         self.assertEqual(calculate('2 * 3 + 5 / 6 * 3 + 15'), 23.5)
 
+    def test_given_example_2(self):
+        self.assertEqual(calculate('2 - 6 - 7 * 8 / 2 + 5'), -27)
 
+    def test_given_low_priority_operator_in_the_middle_of_high_priority_operators(self):
+        self.assertEqual(calculate('10 - 7 * 8 + 4 / 2 + 5'), -39)
 
 
 if __name__ == '__main__':
