77ed57811899f1d43e0952e7eccaa5791299bec4
Ruhong <seahrh@gmail.com>
2019-06-27 16:19:57 +0800

    remove invalid parentheses: DFS backtracking works

--- recursion/remove_invalid_parentheses.py
+++ recursion/remove_invalid_parentheses.py
@@ -16,69 +16,56 @@ Example 3:
 Input: ")("
 Output: [""]
 
-From https://leetcode.com/articles/remove-invalid-parentheses/
+From
+- https://leetcode.com/articles/remove-invalid-parentheses/
+- https://kennyzhuang.gitbooks.io/algorithms-collection/content/remove_invalid_parentheses1.html
+
 SOLUTION
+DFS backtracking
+Limit max removal rmL and rmR for backtracking boundary.
+Otherwise it will exhaust all possible valid substrings, not shortest ones.
+Scan from left to right, avoiding invalid strs (on the fly) by checking num of open parens.
+If it's '(', either keep it or drop it.
+If it's ')', either keep it or drop it.
+Otherwise just append it.
+
+Time O(2^N): since in the worst case we will have only left parentheses
+in the expression and for every bracket we will have two options
+i.e. whether to remove it or consider it.
+Space O(N): recursive call stack
 """
-import sys
 
 
-def _remove(string, index, left_count, right_count, expr, rem_count, valid_expressions, min_removed):
-    """
-        string: The original string we are recursing on.
-        index: current index in the original string.
-        left: number of left parentheses till now.
-        right: number of right parentheses till now.
-        ans: the resulting expression in this particular recursion.
-        ignored: number of parentheses ignored in this particular recursion.
-    """
-    # Base case: reached the end of string.
-    if index == len(string):
-        # If the current expression is valid. The only scenario where it can be
-        # invalid here is if left > right. The other way around we handled early on in the recursion.
-        if left_count == right_count:
-            if rem_count <= min_removed:
-                # This is the resulting expression.
-                # Strings are immutable in Python so we move around a list in the recursion
-                # and eventually join to get the final string.
-                possible_ans = ''.join(expr)
-
-                # If the current count of brackets removed < current minimum, ignore
-                # previous answers and update the current minimum count.
-                if rem_count < min_removed:
-                    valid_expressions = set()
-                    min_removed = rem_count
-                valid_expressions.add(possible_ans)
+def _remove(result, string, index, left, right, openn, partial):
+    if index == len(string) and left == 0 and right == 0 and openn == 0:
+        result.add(partial)
         return
-    current_char = string[index]
-    # If the current character is not a parenthesis, just recurse one step ahead.
-    if current_char != '(' and current_char != ')':
-        expr.append(current_char)
-        _remove(string, index + 1, left_count, right_count, expr, rem_count,
-                valid_expressions=valid_expressions, min_removed=min_removed)
-        expr.pop()
+    if index == len(string) or left < 0 or right < 0 or openn < 0:
         return
-    # Else, one recursion is with ignoring the current character.
-    # So, we increment the ignored counter and leave the left and right untouched.
-    _remove(string, index + 1, left_count, right_count, expr, rem_count + 1,
-            valid_expressions=valid_expressions, min_removed=min_removed)
-    expr.append(current_char)
-
-    # If the current parenthesis is an opening bracket, we consider it
-    # and increment left and  move forward
-    if string[index] == '(':
-        _remove(string, index + 1, left_count + 1, right_count, expr, rem_count,
-                valid_expressions=valid_expressions, min_removed=min_removed)
-    elif right_count < left_count:
-        # If the current parenthesis is a closing bracket, we consider it only if we
-        # have more number of opening brackets and increment right and move forward.
-        _remove(string, index + 1, left_count, right_count + 1, expr, rem_count,
-                valid_expressions=valid_expressions, min_removed=min_removed)
-    expr.pop()
+    c = string[index]
+    longer = partial + c
+    if c == '(':
+        _remove(result, string, index + 1, left - 1, right, openn, partial)  # drop it
+        _remove(result, string, index + 1, left, right, openn + 1, longer)  # keep it
+        return
+    if c == ')':
+        _remove(result, string, index + 1, left, right - 1, openn, partial)  # drop it
+        _remove(result, string, index + 1, left, right, openn - 1, longer)  # keep it
+        return
+    _remove(result, string, index + 1, left, right, openn, longer)
 
 
 def remove(string):
-    valid_expressions = set()
-    min_removed = sys.maxsize
-    _remove(string, index=0, left_count=0, right_count=0, expr=[], rem_count=0,
-            valid_expressions=valid_expressions, min_removed=min_removed)
-    return list(valid_expressions)
+    res = set()
+    left = 0
+    right = 0
+    for c in string:
+        if c == '(':
+            left += 1
+        if c == ')':
+            if left != 0:
+                left -= 1
+            else:
+                right += 1
+    _remove(res, string, 0, left, right, 0, '')
+    return list(res)
--- recursion/tests/test_remove_invalid_parentheses.py
+++ recursion/tests/test_remove_invalid_parentheses.py
@@ -3,15 +3,18 @@ from recursion.remove_invalid_parentheses import *
 
 
 class TestRemoveInvalidParentheses(unittest.TestCase):
-    @unittest.skip
+
     def test_given_empty_string_then_return_empty_list(self):
-        self.assertListEqual(remove(''), [''])
+        self.assertCountEqual(remove(''), [''])
 
-    @unittest.skip
     def test_given_string_starts_with_closing_bracket_then_return_empty_list(self):
-        self.assertListEqual(remove(')('), [''])
+        self.assertCountEqual(remove(')('), [''])
 
+    def test_given_at_least_one_left_bracket_and_one_right_bracket_in_excess(self):
+        self.assertCountEqual(remove('()())()'), ["()()()", "(())()"])
 
+    def test_given_non_bracket_characters_exist(self):
+        self.assertCountEqual(remove('(a)())()'), ["(a())()", "(a)()()"])
 
 
 if __name__ == '__main__':
