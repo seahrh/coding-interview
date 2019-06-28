a383e706391d2e72b55bc0ac1345f18fd46be52d
Ruhong <seahrh@gmail.com>
2019-06-28 15:59:27 +0800

    refactor rename vars

--- sequences/duplicates.py
+++ sequences/duplicates.py
@@ -7,11 +7,11 @@ def remove_duplicates(s):
     # This takes O(n) time and O(m) space, where m is the number of unique chars.
     # assume UTF-8 ASCII charset only
     num_chars = 128
-    flags = [False] * num_chars
+    is_present = [False] * num_chars
     res = ''
     for c in s:
         cp = ord(c)
-        if flags[cp] is False:
+        if is_present[cp] is False:
             res += c
-            flags[cp] = True
+            is_present[cp] = True
     return res
