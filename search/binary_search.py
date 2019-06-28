7317c585baf7c6e674d1211446203ed5743b4338
Ruhong <seahrh@gmail.com>
2019-06-28 13:57:26 +0800

    refactor rename vars

--- search/binary_search.py
+++ search/binary_search.py
@@ -23,43 +23,43 @@ def search_array_with_empty_strings(arr, key):
     # [CTCI Q9.5]
     if len(arr) == 0:
         raise ValueError('arr must not be empty')
-    begin = 0
-    end = len(arr) - 1
-    while begin <= end:
+    lo = 0
+    hi = len(arr) - 1
+    while lo <= hi:
         # ensure there is something at the end
-        while begin <= end and arr[end] == '':
-            end -= 1
-        if end < begin:
+        while lo <= hi and arr[hi] == '':
+            hi -= 1
+        if hi < lo:
             return -1
-        mid = int((end - begin) / 2 + begin)  # avoid integer overflow
+        mid = int((hi - lo) / 2 + lo)  # avoid integer overflow
         # Keep going right until a non-empty string is found
         while arr[mid] == '':
             mid += 1
         if arr[mid] == key:
             return mid
         if key > arr[mid]:
-            begin = mid + 1
+            lo = mid + 1
             continue
         if key < arr[mid]:
-            end = mid - 1
+            hi = mid - 1
     return -1
 
 
 def nearest(arr, key):
     if len(arr) == 0:
         raise ValueError('arr must not be empty')
-    begin = 0
-    end = len(arr) - 1
+    lo = 0
+    hi = len(arr) - 1
     res = 0
-    while begin <= end:
-        mid = int((end - begin) / 2 + begin)  # avoid integer overflow
+    while lo <= hi:
+        mid = int((hi - lo) / 2 + lo)  # avoid integer overflow
         if arr[mid] == key:
             return mid
         if fabs(arr[mid] - key) < fabs(arr[res] - key):
             res = mid
         if key > arr[mid]:
-            begin = mid + 1
+            lo = mid + 1
             continue
         if key < arr[mid]:
-            end = mid - 1
+            hi = mid - 1
     return res
