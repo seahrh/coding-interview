d2f62d97ecf95c8867971b888735a0aea28380f5
Ruhong <seahrh@gmail.com>
2019-06-28 16:47:39 +0800

    put solution comments at top

--- sequences/longest_balanced_subsequence.py
+++ sequences/longest_balanced_subsequence.py
@@ -3,6 +3,22 @@ Letters and Numbers: Given an array filled with letters and numbers, find the lo
 an equal number of letters and numbers.
 
 (17.5, p547)
+Solution:
+Study the numbers before the subarray (4, 2) and the end (7, 5).
+You might notice that, while the values aren't the same,
+the differences are: 4 - 2 = 7 - 5. This makes sense.
+Since they've added the same number of letters and numbers,
+they should maintain the same difference.
+
+Whenever we return the same difference, then we know we have found an equal subarray.
+To find the biggest subarray, we just have to find the two indices
+farthest apart with the same value.
+
+To do so, we use a hash table to store the first time we see a particular difference.
+Then, each time we see the same difference, check if this subarray is longer than the current max.
+If so, we update the max.
+
+O(n) time and O(n) space (to hold the deltas and result).
 """
 
 
@@ -24,7 +40,10 @@ def _deltas(arr, is_left, is_right):
 
 def _longest_match(deltas):
     """Find the matching pair of values in the deltas array
-    with the largest difference in indices."""
+    with the largest difference in indices.
+
+    Returns tuple (lo, hi), not inclusive of hi
+    """
     _map = {0: -1}  # seed distance zero at index -1
     res = None
     for index, d in enumerate(deltas):
@@ -39,16 +58,7 @@ def _longest_match(deltas):
 
 
 def longest_balanced_subsequence(arr, is_left, is_right):
-    """Solution:
-    Whenever we return the same difference, then we know we have found an equal subarray.
-    To find the biggest subarray, we just have to find the two indices farthest apart with the same value.
-
-    To do so, we use a hash table to store the first time we see a particular difference.
-    Then, each time we see the same difference, check if this subarray is longer than the current max.
-    If so, we update the max.
-
-    O(n) time and O(n) space (to hold the deltas and result).
-
+    """
     :param arr: array of characters belonging to either LEFT or RIGHT groups
     :param is_left: function to determine if char belongs to LEFT group
     :param is_right: function to determine if char belongs to RIGHT group
