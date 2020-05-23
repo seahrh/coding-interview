dffef9b3e8ba00d46dda742ba92dd83b2b4ed9bb
Ruhong <seahrh@gmail.com>
2020-05-23 16:22:13 +0800

    tower of hanoi: add typing

--- src/recursion/tests/test_tower_of_hanoi.py
+++ src/recursion/tests/test_tower_of_hanoi.py
@@ -1,32 +1,32 @@
 from recursion.tower_of_hanoi import *
 
 
-class TestTowerOfHanoi:
+class TestTowerOfHanoiRecursive:
     def test_move_1_disk(self):
-        fromm = [1]
-        to = []
-        buf = []
-        move(len(fromm), fromm=fromm, to=to, buf=buf)
-        assert to == [1]
-        assert fromm == []
-        assert buf == []
+        fs = [1]
+        ts = []
+        bs = []
+        solve(len(fs), from_stack=fs, to_stack=ts, buffer_stack=bs)
+        assert ts == [1]
+        assert fs == []
+        assert bs == []
 
     def test_move_3_disks(self):
-        fromm = [i for i in range(1, 4)]
-        fromm.reverse()
-        to = []
-        buf = []
-        move(len(fromm), fromm=fromm, to=to, buf=buf)
-        assert to == [3, 2, 1]
-        assert fromm == []
-        assert buf == []
+        fs = [i for i in range(1, 4)]
+        fs.reverse()
+        ts = []
+        bs = []
+        solve(len(fs), from_stack=fs, to_stack=ts, buffer_stack=bs)
+        assert ts == [3, 2, 1]
+        assert fs == []
+        assert bs == []
 
     def test_move_8_disks(self):
-        fromm = [i for i in range(1, 9)]
-        fromm.reverse()
-        to = []
-        buf = []
-        move(len(fromm), fromm=fromm, to=to, buf=buf)
-        assert to == [8, 7, 6, 5, 4, 3, 2, 1]
-        assert fromm == []
-        assert buf == []
+        fs = [i for i in range(1, 9)]
+        fs.reverse()
+        ts = []
+        bs = []
+        solve(len(fs), from_stack=fs, to_stack=ts, buffer_stack=bs)
+        assert ts == [8, 7, 6, 5, 4, 3, 2, 1]
+        assert fs == []
+        assert bs == []
--- src/recursion/tower_of_hanoi.py
+++ src/recursion/tower_of_hanoi.py
@@ -1,9 +1,18 @@
-def move(n, fromm, to, buf):
-    # Move a tower of height-1 to an intermediate pole, using the final pole.
-    # Move the remaining disk to the final pole.
-    # Move the tower of height-1 from the intermediate pole to the final pole using the original pole.
+from typing import List
+
+
+def solve(
+    n: int, from_stack: List[int], to_stack: List[int], buffer_stack: List[int]
+) -> None:
+    """
+    Move a tower of height-1 to an intermediate pole, using the final pole.
+    Move the remaining disk to the final pole.
+    Move the tower of height-1 from the intermediate pole to the final pole using the original pole.
+    Time O(2^N)
+    Space O(N): recursive call stack
+    """
     if n == 0:  # Base case: no disk to move
         return
-    move(n - 1, fromm, buf, to)
-    to.append(fromm.pop())
-    move(n - 1, buf, to, fromm)
+    solve(n - 1, from_stack, buffer_stack, to_stack)
+    to_stack.append(from_stack.pop())
+    solve(n - 1, buffer_stack, to_stack, from_stack)
