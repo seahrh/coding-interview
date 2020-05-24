2054cf354c3e0067ff224acb86177eb3caa7e34d
Ruhong <seahrh@gmail.com>
2020-05-24 20:00:35 +0800

    refactor move string permutations to backtracking

--- src/backtracking/string_permutations.py
+++ src/backtracking/string_permutations.py
@@ -2,22 +2,25 @@
 Permutations without Dups: Write a method to compute all permutations of a string of unique
 characters.
 (8.7, p367)
+
+SOLUTION
+Backtracking without pruning: enumerate all possibilities.
+Time O(N * N!)
+- N recursive calls.
+- Inserting the character takes factorial time because there are N! permutations (partials).
+Space O(N): recursive call stack
 """
+from typing import Set
 
 
-def permutate_without_duplicates(remainder):
-    """
-    There are n recursive calls.
-    Inserting the character takes factorial time because there are n! permutations.
-    This takes O(n * n!) time and O(n) space.
-    """
-    res = []
+def permutate_without_duplicates(remainder: str) -> Set[str]:
+    res = set()
     if len(remainder) == 0:  # base case
-        res.append("")
+        res.add("")
         return res
     head = remainder[0]
     partials = permutate_without_duplicates(remainder[1:])
     for p in partials:
         for i in range(len(p) + 1):  # also insert character at the end of the string!
-            res.append(p[:i] + head + p[i:])
+            res.add(p[:i] + head + p[i:])
     return res
--- src/backtracking/tests/test_string_permutations.py
+++ src/backtracking/tests/test_string_permutations.py
@@ -1,28 +1,28 @@
-from recursion.string_permutations import *
+from backtracking.string_permutations import *
 
 
 class TestStringPermutationsWithoutDuplicates:
     def test_when_string_is_empty_then_return_array_with_an_empty_string(self):
-        assert permutate_without_duplicates("") == [""]
+        assert permutate_without_duplicates("") == {""}
 
     def test_when_string_is_length_one(self):
-        assert permutate_without_duplicates("a") == ["a"]
+        assert permutate_without_duplicates("a") == {"a"}
 
     def test_when_string_is_length_two(self):
-        assert permutate_without_duplicates("ab") == ["ab", "ba"]
+        assert permutate_without_duplicates("ab") == {"ab", "ba"}
 
     def test_when_string_is_length_three(self):
-        assert permutate_without_duplicates("abc") == [
+        assert permutate_without_duplicates("abc") == {
             "abc",
             "bac",
             "bca",
             "acb",
             "cab",
             "cba",
-        ]
+        }
 
     def test_when_string_is_length_four(self):
-        assert permutate_without_duplicates("abcd") == [
+        assert permutate_without_duplicates("abcd") == {
             "abcd",
             "bacd",
             "bcad",
@@ -47,4 +47,4 @@ class TestStringPermutationsWithoutDuplicates:
             "dacb",
             "dcab",
             "dcba",
-        ]
+        }
