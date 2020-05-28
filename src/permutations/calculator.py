afd4bb438c8302736af06cfd1b80ecf1c469f30f
Ruhong <seahrh@gmail.com>
2020-05-28 13:27:22 +0800

    calculator: add typing

--- src/permutations/calculator.py
+++ src/permutations/calculator.py
@@ -7,8 +7,11 @@ Output: 23.5
 
 (16.26, p535)
 Solution with two stacks (1 stack for operators and 1 stack for operands)
-takes O(n) time and O(n) space, where n is the length of the expression to evaluate.
+N is the length of the expression to evaluate.
+Time O(N)
+Space O(N)
 """
+from typing import List, Optional
 from enum import Enum
 
 
@@ -20,7 +23,7 @@ class Operator(Enum):
     NOOP = ""
 
     @staticmethod
-    def parse(string):
+    def parse(string: str) -> Optional["Operator"]:
         if string == Operator.ADD.value:
             return Operator.ADD
         if string == Operator.SUBTRACT.value:
@@ -31,7 +34,7 @@ class Operator(Enum):
             return Operator.DIVIDE
         return None
 
-    def priority(self):
+    def priority(self) -> int:
         if self == Operator.ADD:
             return 1
         if self == Operator.SUBTRACT:
@@ -42,7 +45,7 @@ class Operator(Enum):
             return 2
         return 0
 
-    def apply(self, left, right):
+    def apply(self, left: float, right: float) -> float:
         if self == Operator.ADD:
             return left + right
         if self == Operator.SUBTRACT:
@@ -54,26 +57,28 @@ class Operator(Enum):
         return right
 
 
-def _collapse(future_top, operand_stack, operator_stack):
+def _collapse(
+    future_top: Operator, operand_stack: List[float], operator_stack: List[Operator]
+) -> None:
     """Collapse top until priority(futureTop) > priority(top).
     Collapsing means to pop the top 2 numbers and
     apply the operator popped from the top of the operator stack,
     and then push that onto the numbers stack."""
     while len(operator_stack) >= 1 and len(operand_stack) >= 2:
         if future_top.priority() > operator_stack[-1].priority():
-            break
+            return
         right = operand_stack.pop()
         left = operand_stack.pop()
         res = operator_stack.pop().apply(left, right)
         operand_stack.append(res)
 
 
-def calculate(string):
-    if len(string.strip()) < 6:
+def calculate(expression: str) -> float:
+    if len(expression.strip()) < 6:
         raise ValueError("Expression must have at least one operator and two operands")
-    operand_stack = []
-    operator_stack = []
-    for term in string.split():
+    operand_stack: List[float] = []
+    operator_stack: List[Operator] = []
+    for term in expression.split():
         op = Operator.parse(term)
         if op is None:
             operand_stack.append(float(term))
