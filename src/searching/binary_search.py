4d44da95328cfee39598fbd5d2b8e3eaabf25062
Ruhong <seahrh@gmail.com>
2020-06-09 14:22:25 +0800

    binary search: add typing

--- src/searching/binary_search.py
+++ src/searching/binary_search.py
@@ -1,9 +1,8 @@
-from math import fabs
+import math
+from typing import List
 
 
-def search(arr, key):
-    if len(arr) == 0:
-        raise ValueError("arr must not be empty")
+def search(arr: List[int], key: int) -> int:
     lo = 0
     hi = len(arr) - 1
     while lo <= hi:
@@ -18,11 +17,9 @@ def search(arr, key):
     return -1
 
 
-def search_array_with_empty_strings(arr, key):
+def search_array_with_empty_strings(arr: List[str], key: str) -> int:
     # Search a sorted array of strings which is interspersed with empty strings.
     # [CTCI Q9.5]
-    if len(arr) == 0:
-        raise ValueError("arr must not be empty")
     lo = 0
     hi = len(arr) - 1
     while lo <= hi:
@@ -31,7 +28,7 @@ def search_array_with_empty_strings(arr, key):
             hi -= 1
         if hi < lo:
             return -1
-        mid = int((hi - lo) / 2 + lo)  # avoid integer overflow
+        mid = int(lo / 2 + hi / 2)  # avoid integer overflow
         # Keep going right until a non-empty string is found
         while arr[mid] == "":
             mid += 1
@@ -45,17 +42,15 @@ def search_array_with_empty_strings(arr, key):
     return -1
 
 
-def nearest(arr, key):
-    if len(arr) == 0:
-        raise ValueError("arr must not be empty")
+def nearest(arr: List[int], key: int) -> int:
     lo = 0
     hi = len(arr) - 1
     res = 0
     while lo <= hi:
-        mid = int((hi - lo) / 2 + lo)  # avoid integer overflow
+        mid = int(lo / 2 + hi / 2)  # avoid integer overflow
         if arr[mid] == key:
             return mid
-        if fabs(arr[mid] - key) < fabs(arr[res] - key):
+        if math.fabs(arr[mid] - key) < math.fabs(arr[res] - key):
             res = mid
         if key > arr[mid]:
             lo = mid + 1
