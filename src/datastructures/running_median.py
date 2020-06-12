cf17897f7cfbc835f845c27f5214180c7d59167a
Ruhong <seahrh@gmail.com>
2020-06-12 17:05:02 +0800

    running median: add typing

--- src/datastructures/running_median.py
+++ src/datastructures/running_median.py
@@ -1,48 +1,52 @@
 """
 17.20 Continuous Median: Numbers are randomly generated and passed to a method.
 Write a program to find and maintain the median value as new values are generated.
+
+SOLUTION
+Use two priority heaps: a max heap for the values below the median,
+and a min heap for the values above the median.
+
+When a new value arrives,
+it is placed in the maxHeap if the value is less than or equal to the median,
+otherwise it is placed into the minHeap.
+
+The heap sizes can be equal, or the maxHeap may have one extra element.
+This constraint can easily be restored by shifting an element from one heap to the other.
+The median takes O(1) time, by looking at the top element(s).
+Updates take O(lg n) time.
+The heaps take O(n) space.
+
+Max heap is implemented in Python by multiplying the value by -1
+before adding it to the heap ("taking the inverse").
 """
-import heapq
+from heapq import heappushpop, heappush
+from typing import Union, Generic, List
+
+T = Union[float, int]
 
 
 class RunningMedian:
-    """Use two priority heaps: a max heap for the values below the median,
-    and a min heap for the values above the median.
-
-    When a new value arrives,
-    it is placed in the maxHeap if the value is less than or equal to the median,
-    otherwise it is placed into the minHeap.
-    The heap sizes can be equal, or the maxHeap may have one extra element.
-    This constraint can easily be restored by shifting an element from one heap to the other.
-    The median takes O(1) time, by looking at the top element(s).
-    Updates take O(lg n) time.
-    The heaps take O(n) space.
-
-    Max heap is implemented in Python by multiplying the value by -1
-    before adding it to the heap ("taking the inverse").
-    """
-
     def __init__(self):
-        self.min_heap = []
-        self.max_heap = []
+        self.min_heap: List[T] = []
+        self.max_heap: List[T] = []
 
-    def add(self, *values):
+    def add(self, *values: T) -> None:
         """Maintains invariant that len(max_heap) >= len(min_heap)"""
         for v in values:
             if len(self.max_heap) == len(self.min_heap):
                 if len(self.min_heap) and v > self.min_heap[0]:
-                    u = heapq.heappushpop(self.min_heap, v)
-                    heapq.heappush(self.max_heap, u * -1)
+                    u = heappushpop(self.min_heap, v)
+                    heappush(self.max_heap, u * -1)
                 else:
-                    heapq.heappush(self.max_heap, v * -1)
+                    heappush(self.max_heap, v * -1)
             else:
                 if v < self.max_heap[0]:
-                    u = heapq.heappushpop(self.max_heap, v * -1) * -1
-                    heapq.heappush(self.min_heap, u)
+                    u = heappushpop(self.max_heap, v * -1) * -1
+                    heappush(self.min_heap, u)
                 else:
-                    heapq.heappush(self.min_heap, v)
+                    heappush(self.min_heap, v)
 
-    def median(self):
+    def median(self) -> T:
         # maxHeap is always at least as big as minHeap.
         # So if maxHeap is empty, then minHeap is also empty.
         if len(self.max_heap) == 0:
