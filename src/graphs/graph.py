969d8d91c30526452135edef8a2bd50f5fc1ee5a
Ruhong <seahrh@gmail.com>
2020-07-01 15:55:13 +0800

    refactor extract module: topological sort

--- src/graphs/graph.py
+++ src/graphs/graph.py
@@ -11,7 +11,6 @@ from typing import (
     FrozenSet,
     Hashable,
     Dict,
-    Optional,
 )
 
 T = TypeVar("T", bound=Hashable)  # Declare type variable
@@ -151,94 +150,6 @@ class DiGraph(Generic[T]):
                     discovered.add(a)
                     st.append(a)
         return res
-
-    def find_cycle(self) -> "DiGraph[T]":
-        """Returns one cycle in the graph if it exists.
-        Explored means that a node and all its descendants have been explored.
-        White set: nodes that have not been explored
-        Gray set: nodes that are undergoing exploration but not completed
-        Black set: nodes that are completely explored
-
-        Time O(V + E): same as DFS
-        Space O(V): stack covers all nodes
-        Based on https://www.youtube.com/watch?v=rKQaZuoUR4M
-        """
-        res: "DiGraph[T]" = DiGraph[T]()
-        white: Set[T] = set(self.nodes())
-        gray: Set[T] = set()
-        black: Set[T] = set()
-        st: List[T] = []  # dfs
-        parent: Dict[T, Optional[T]] = {}  # map of child node to parent node
-        while len(white) != 0 or len(st) != 0:
-            if len(st) == 0:
-                # add next node from white set
-                # both white set and stack cannot be empty at the same time.
-                tmp = white.pop()
-                gray.add(tmp)
-                st.append(tmp)
-                parent[tmp] = None
-            curr = st[-1]  # do not pop the top!
-            explored = True
-            for a in self.adjacent(curr):
-                if a in black:
-                    continue
-                if a in gray:  # cycle found
-                    left: Optional[T] = curr
-                    right: T = a
-                    while left is not None:
-                        res.add(Edge(left, right))
-                        right = left
-                        left = parent[left]
-                    return res
-                explored = False
-                parent[a] = curr
-                white.discard(a)
-                gray.add(a)
-                st.append(a)
-            if explored:
-                st.pop()  # pop only if the node and its successors have been explored
-                gray.remove(curr)
-                black.add(curr)
-        return res
-
-    def has_cycle(self) -> bool:
-        return len(self.find_cycle()) != 0
-
-    def topsort(self) -> List[T]:
-        """Topological sort orders the vertices on a line such that all directed edges go from left to right.
-
-        Time O(V + E): DFS traversal
-        Space O(V)
-        """
-        ordering: Deque[T] = deque()  # result is a queue; insert from left
-        st: List[T] = []  # DFS iteration
-        white: Set[T] = set(self.nodes())
-        gray: Set[T] = set()
-        black: Set[T] = set()
-        while len(white) != 0 or len(st) != 0:
-            if len(st) == 0:
-                # add next node from white set
-                # both white set and stack cannot be empty at the same time.
-                tmp = white.pop()
-                gray.add(tmp)
-                st.append(tmp)
-            curr = st[-1]  # do not pop the top!
-            explored = True
-            for a in self.adjacent(curr):
-                if a in black:
-                    continue
-                if a in gray:  # cycle found; topological sort is not possible
-                    return []
-                explored = False
-                white.discard(a)
-                gray.add(a)
-                st.append(a)
-            if explored:
-                st.pop()  # pop only if the node and its successors have been explored
-                gray.remove(curr)
-                black.add(curr)
-                ordering.appendleft(curr)
-        return list(ordering)
 
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, dict(self._alist))
