8fc6b989af1e76d065424b5b9d983ec97adf86d3
Ruhong <seahrh@gmail.com>
2020-07-07 11:29:42 +0800

    minor change

--- src/geometry/distances.py
+++ src/geometry/distances.py
@@ -6,10 +6,10 @@ T = TypeVar("T")
 
 
 def euclidean_distance(p: List[float], q: List[float]) -> float:
-    """The Euclidean distance or Euclidean metric is the "ordinary" straight-line distance
+    """Square root of the sum of squared differences.
+    The Euclidean distance or Euclidean metric is the "ordinary" straight-line distance
     between two points in Euclidean space. With this distance, Euclidean space becomes a metric space.
-    The associated norm is called the Euclidean norm.
-    A generalized term for the Euclidean norm is the L2 norm or L2 distance.
+    The associated norm is called the Euclidean norm, L2 norm or L2 distance.
     """
     if p is None or len(p) == 0:
         raise ValueError("p must not be None or empty")
@@ -17,11 +17,10 @@ def euclidean_distance(p: List[float], q: List[float]) -> float:
         raise ValueError("q must not be None or empty")
     if len(p) != len(q):
         raise ValueError("vectors p and q must have the same dimension")
-    res: float = 0
+    _sum: float = 0
     for i in range(len(p)):
-        res += (q[i] - p[i]) ** 2
-    res = math.sqrt(res)
-    return res
+        _sum += (q[i] - p[i]) ** 2
+    return math.sqrt(_sum)
 
 
 def magnitude(p: Iterable[float]) -> float:
