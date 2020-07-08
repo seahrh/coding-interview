3eb19e46215ab9647a0abb3609c043d8a6ea5b91
Ruhong <seahrh@gmail.com>
2020-07-09 04:07:00 +0800

    matrix dot product

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,7 +1,8 @@
-from typing import Iterable, TypeVar
+from typing import Iterable, TypeVar, List, Union
 
 
 T = TypeVar("T")
+Numeric = Union[int, float]
 
 
 def argmin(elements: Iterable[T]) -> int:
@@ -12,3 +13,17 @@ def argmin(elements: Iterable[T]) -> int:
 def argmax(elements: Iterable[T]) -> int:
     """Returns first index of largest element."""
     return max(enumerate(elements), key=lambda x: x[1])[0]
+
+
+def dot(p: List[List[Numeric]], q: List[List[Numeric]]) -> List[List[float]]:
+    """Matrix dot product."""
+    p_shape = len(p), len(p[0])
+    q_shape = len(q), len(q[0])
+    if p_shape[1] != q_shape[0]:
+        raise ValueError("number of columns in p must equal the number of rows in q")
+    res: List[List[float]] = [[0.0] * q_shape[1] for _ in range(p_shape[0])]
+    for i in range(p_shape[0]):
+        for j in range(q_shape[1]):
+            for k in range(p_shape[1]):
+                res[i][j] += p[i][k] * q[k][j]
+    return res
--- src/util/tests/test_util.py
+++ src/util/tests/test_util.py
@@ -35,3 +35,52 @@ class TestArgMax:
         assert argmax([False, True, True, False]) == 1
         assert argmax([True, False, False, True]) == 0
         assert argmax([False, False, True]) == 2
+
+
+class TestDotProduct:
+    def test_matrix_with_1_row(self):
+        assert dot([[1]], [[-1]]) == [[-1]]
+        assert dot([[-1]], [[1]]) == [[-1]]
+        assert dot([[1]], [[2]]) == [[2]]
+        assert dot([[2]], [[1]]) == [[2]]
+
+    def test_matrix_with_2_rows(self):
+        p = [[1, 2], [3, 4]]
+        q = [[1], [2]]
+        assert dot(p, q) == [[5], [11]]
+        p = [[1, 2], [3, 4]]
+        q = [[3, 1], [2, 4]]
+        assert dot(p, q) == [[7, 9], [17, 19]]
+        assert dot(q, p) == [[6, 10], [14, 20]]
+        p = [[1, 2], [3, 4]]
+        q = [[2, 0], [1, 2]]
+        assert dot(p, q) == [[4, 4], [10, 8]]
+        assert dot(q, p) == [[2, 4], [7, 10]]
+
+    def test_matrix_with_3_rows(self):
+        p = [[1, 2, 3], [4, 5, 6]]
+        q = [[7, 8], [9, 10], [11, 12]]
+        assert dot(p, q) == [[58, 64], [139, 154]]
+        p = [[3, 4, 2]]
+        q = [[13, 9, 7, 15], [8, 7, 4, 6], [6, 4, 0, 3]]
+        assert dot(p, q) == [[83, 63, 37, 75]]
+        p = [[1, 2, 3]]
+        q = [[4], [5], [6]]
+        assert dot(p, q) == [[32]]
+        assert dot(q, p) == [[4, 8, 12], [5, 10, 15], [6, 12, 18]]
+        p = [[2, 0, -1], [3, 5, 2], [-4, 1, 4]]
+        q = [[5, 1, -2], [-1, 0, 4], [2, -3, 3]]
+        assert dot(p, q) == [
+            [8, 5, -7],
+            [14, -3, 20],
+            [-13, -16, 24],
+        ]
+        assert dot(q, p) == [
+            [21.0, 3.0, -11.0],
+            [-18.0, 4.0, 17.0],
+            [-17.0, -12.0, 4.0],
+        ]
+        p = [[3, -2, 5], [0, -1, 6], [-4, 2, -1]]
+        q = [[2, -1, 0], [3, -5, 2], [1, 4, -2]]
+        assert dot(p, q) == [[5.0, 27.0, -14.0], [3.0, 29.0, -14.0], [-3.0, -10.0, 6.0]]
+        assert dot(q, p) == [[6.0, -3.0, 4.0], [1.0, 3.0, -17.0], [11.0, -10.0, 31.0]]
