48cf68ecff2cdc2969c0cb7753e72f597fbdac65
Ruhong <seahrh@gmail.com>
2020-07-31 21:50:15 +0800

    find two duplicates in 1..N

--- src/searching/duplicates.py
+++ src/searching/duplicates.py
@@ -11,10 +11,6 @@ Array length is N+1
 2
 
 
-[1, 3, 4, 2, 2]
-2
-
-
 Sum(1..N) = sum_n
 Sum(array) - sum_n = duplicate_number
 Time O(N)
@@ -26,16 +22,37 @@ Space O(1)
 
 [2, 1, 2, 3, 4]
 i=0
+
+A more realistic problem would be to find a variable number of duplicates
+but the space cost must be relaxed to O(N) to hold the result.
+Hence the question asks for a fixed number of duplicates.
 """
-from typing import List
+from typing import List, Tuple
 
 
-def find_one_duplicate(arr: List[int]) -> int:
-    while True:
-        # temp variable is required to do the swap properly!
+def find_one(arr: List[int]) -> int:
+    res: int = 0
+    for _ in arr:
+        # temp variable `res` is required to do the swap properly!
         # the following swap does not work!
         # arr[0], arr[arr[0]] = arr[arr[0]], arr[0]
-        tmp = arr[0]
-        if tmp == arr[tmp]:
-            return tmp
-        arr[0], arr[tmp] = arr[tmp], arr[0]
+        res = arr[0]
+        if res == arr[res]:
+            break
+        arr[0], arr[res] = arr[res], arr[0]
+    return res
+
+
+def find_two(arr: List[int]) -> Tuple[int, int]:
+    first = 0
+    second = 0
+    for a in arr:
+        if arr[abs(a)] < 0:
+            if first == 0:
+                first = abs(a)
+            elif second == 0:
+                second = abs(a)
+                break
+            continue  # do not update the negated value again
+        arr[abs(a)] *= -1
+    return first, second
--- src/searching/tests/test_duplicates.py
+++ src/searching/tests/test_duplicates.py
@@ -3,13 +3,29 @@ from searching.duplicates import *
 
 class TestFindOneDuplicate:
     def test_shortest_array(self):
-        assert find_one_duplicate([1, 1]) == 1
+        assert find_one([1, 1]) == 1
 
     def test_duplicate_in_the_middle(self):
-        assert find_one_duplicate([1, 1, 2]) == 1
-        assert find_one_duplicate([3, 1, 3, 4, 2]) == 3
+        assert find_one([1, 1, 2]) == 1
+        assert find_one([3, 1, 3, 4, 2]) == 3
 
     def test_duplicate_at_the_end(self):
-        assert find_one_duplicate([1, 2, 2]) == 2
-        assert find_one_duplicate([1, 2, 1]) == 1
-        assert find_one_duplicate([1, 3, 4, 2, 2]) == 2
+        assert find_one([1, 2, 2]) == 2
+        assert find_one([1, 2, 1]) == 1
+        assert find_one([1, 3, 4, 2, 2]) == 2
+
+
+class TestFindTwoDuplicates:
+    def test_shortest_array(self):
+        assert find_two([1, 1, 2, 2]) == (1, 2)
+        assert find_two([2, 2, 1, 1]) == (2, 1)
+        assert find_two([1, 2, 1, 2]) == (1, 2)
+        assert find_two([2, 1, 2, 1]) == (2, 1)
+
+    def test_duplicate_in_the_middle(self):
+        assert find_two([3, 1, 3, 4, 1, 2]) == (3, 1)
+        assert find_two([3, 1, 1, 3, 4, 2]) == (1, 3)
+
+    def test_duplicate_at_the_end(self):
+        assert find_two([1, 3, 4, 4, 2, 2]) == (4, 2)
+        assert find_two([2, 1, 4, 3, 4, 2]) == (4, 2)
