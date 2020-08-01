a8c9f6e688c7d95436e6810e7fcb9208978a50a0
Ruhong <seahrh@gmail.com>
2020-08-01 12:49:59 +0800

    find duplicates: refactor move to sequences package

--- src/sequences/duplicates.py
+++ src/sequences/duplicates.py
@@ -1,9 +1,13 @@
-# Design an algorithm and write code to remove the duplicate characters in
-# a string without using any additional buffer. NOTE: One or two additional
-# variables are fine. An extra copy of the array is not. [CTCI Q1.3]
+from typing import List, Tuple
+
+"""
+Design an algorithm and write code to remove the duplicate characters in
+a string without using any additional buffer. NOTE: One or two additional
+variables are fine. An extra copy of the array is not. [CTCI Q1.3]
+"""
 
 
-def remove_duplicates(s):
+def remove(s):
     # This takes O(n) time and O(m) space, where m is the number of unique chars.
     # assume UTF-8 ASCII charset only
     num_chars = 128
@@ -15,3 +19,62 @@ def remove_duplicates(s):
             res += c
             is_present[cp] = True
     return res
+
+
+"""
+N+1
+Unsorted Array will only have 1..N range inclusive
+Array length is N+1
+
+[3, 1, 3, 4, 2]
+3
+
+
+[1, 3, 4, 2, 2]
+2
+
+
+Sum(1..N) = sum_n
+Sum(array) - sum_n = duplicate_number
+Time O(N)
+Space O(1)
+
+Time O(N)
+Space O(1)
+
+
+[2, 1, 2, 3, 4]
+i=0
+
+A more realistic problem would be to find a variable number of duplicates
+but the space cost must be relaxed to O(N) to hold the result.
+Hence the question asks for a fixed number of duplicates.
+"""
+
+
+def find_one(arr: List[int]) -> int:
+    res: int = 0
+    for _ in arr:
+        # temp variable `res` is required to do the swap properly!
+        # the following swap does not work!
+        # arr[0], arr[arr[0]] = arr[arr[0]], arr[0]
+        res = arr[0]
+        if res == arr[res]:
+            break
+        arr[0], arr[res] = arr[res], arr[0]
+    return res
+
+
+def find_two(arr: List[int]) -> Tuple[int, int]:
+    first = 0
+    second = 0
+    for a in arr:
+        if arr[abs(a)] < 0:
+            if first == 0:
+                first = abs(a)
+            elif second == 0:
+                second = abs(a)
+                break
+            continue  # do not update the negated value again
+        arr[abs(a)] *= -1
+    return first, second
--- src/sequences/tests/test_duplicates.py
+++ src/sequences/tests/test_duplicates.py
@@ -1,17 +1,47 @@
 from sequences.duplicates import *
 
 
-class TestDuplicates:
+class TestRemoveDuplicates:
     def test_empty_strings(self):
-        assert remove_duplicates("") == ""
+        assert remove("") == ""
 
     def test_strings_of_length_one(self):
-        assert remove_duplicates("a") == "a"
+        assert remove("a") == "a"
 
     def test_when_no_duplicates_exist_then_return_original_string(self):
-        assert remove_duplicates("abc") == "abc"
+        assert remove("abc") == "abc"
 
     def test_when_duplicates_exist_then_return_string_without_duplicates(self):
-        assert remove_duplicates("aabc") == "abc"
-        assert remove_duplicates("abca") == "abc"
-        assert remove_duplicates("abac") == "abc"
+        assert remove("aabc") == "abc"
+        assert remove("abca") == "abc"
+        assert remove("abac") == "abc"
+
+
+class TestFindOneDuplicate:
+    def test_shortest_array(self):
+        assert find_one([1, 1]) == 1
+
+    def test_duplicate_in_the_middle(self):
+        assert find_one([1, 1, 2]) == 1
+        assert find_one([3, 1, 3, 4, 2]) == 3
+
+    def test_duplicate_at_the_end(self):
+        assert find_one([1, 2, 2]) == 2
+        assert find_one([1, 2, 1]) == 1
+        assert find_one([1, 3, 4, 2, 2]) == 2
+
+
+class TestFindTwoDuplicates:
+    def test_shortest_array(self):
+        assert find_two([1, 1, 2, 2]) == (1, 2)
+        assert find_two([2, 2, 1, 1]) == (2, 1)
+        assert find_two([1, 2, 1, 2]) == (1, 2)
+        assert find_two([2, 1, 2, 1]) == (2, 1)
+
+    def test_duplicate_in_the_middle(self):
+        assert find_two([3, 1, 3, 4, 1, 2]) == (3, 1)
+        assert find_two([3, 1, 1, 3, 4, 2]) == (1, 3)
+
+    def test_duplicate_at_the_end(self):
+        assert find_two([1, 3, 4, 4, 2, 2]) == (4, 2)
+        assert find_two([2, 1, 4, 3, 4, 2]) == (4, 2)
