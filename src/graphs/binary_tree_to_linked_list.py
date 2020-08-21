560e9068224902925eac7e38e08a3fa14d735f8f
Ruhong <seahrh@gmail.com>
2020-08-21 15:53:28 +0800

    binary tree to linked list: add tests

--- src/graphs/binary_tree_to_linked_list.py
+++ src/graphs/binary_tree_to_linked_list.py
@@ -1,4 +1,6 @@
 """
+Flatten a binary tree into linked list
+========================================
 Given a binary tree, flatten it into linked list in-place. Usage of auxiliary data structure is not allowed.
 After flattening, left of each node should point to NULL and right should contain next node in preorder.
 Examples:
@@ -42,15 +44,21 @@ Output :
              5
 
 See https://www.geeksforgeeks.org/flatten-a-binary-tree-into-linked-list/
+SOLUTION
+Recursively add the linked list of the left subtree
+Time O(N^2): worst case N nodes are found in the left subtree, N nodes have left child.
+Space O(N): depth of recursive call stack
 """
 from typing import Optional
 
 
 class Node:
-    def __init__(self, value: int):
+    def __init__(
+        self, value: int, left: Optional["Node"] = None, right: Optional["Node"] = None
+    ):
         self.value = value
-        self.left: Optional[Node] = None
-        self.right: Optional[Node] = None
+        self.left = left
+        self.right = right
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.value})"
@@ -62,7 +70,7 @@ def solve(root: Node) -> None:
     if root.right is not None:
         solve(root.right)
     leaf: Optional[Node] = root.left
-    while leaf is not None and leaf.right is not None:
+    while leaf is not None and leaf.right is not None:  # Time O(N)
         leaf = leaf.right
     right: Optional[Node] = root.right
     if leaf is not None:
--- src/graphs/tests/test_binary_tree_to_linked_list.py
+++ src/graphs/tests/test_binary_tree_to_linked_list.py
@@ -23,3 +23,31 @@ class TestBinaryTreeToLinkedList:
         assert n1.left is None and n1.right == n2
         assert n2.left is None and n2.right == n3
         assert n3.left is None and n3.right is None
+
+    def test_case_1(self):
+        n3 = Node(3)
+        n4 = Node(4)
+        n6 = Node(6)
+        n2 = Node(2, left=n3, right=n4)
+        n5 = Node(5, right=n6)
+        n1 = Node(1, left=n2, right=n5)
+        solve(n1)
+        assert n1.left is None and n1.right == n2
+        assert n2.left is None and n2.right == n3
+        assert n3.left is None and n3.right == n4
+        assert n4.left is None and n4.right == n5
+        assert n5.left is None and n5.right == n6
+        assert n6.left is None and n6.right is None
+
+    def test_case_2(self):
+        n5 = Node(5)
+        n2 = Node(2, right=n5)
+        n4 = Node(4, left=n2)
+        n3 = Node(3)
+        n1 = Node(1, left=n3, right=n4)
+        solve(n1)
+        assert n1.left is None and n1.right == n3
+        assert n3.left is None and n3.right == n4
+        assert n4.left is None and n4.right == n2
+        assert n2.left is None and n2.right == n5
+        assert n5.left is None and n5.right is None
