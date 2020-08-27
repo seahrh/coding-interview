01cdc0276d6e32ae6e6e5cc34ba4cf5c5c89471d
Ruhong <seahrh@gmail.com>
2020-08-27 11:37:41 +0800

    sliding window maximum: add test

--- src/sortingandsearching/tests/test_sliding_window_maximum.py
+++ src/sortingandsearching/tests/test_sliding_window_maximum.py
@@ -2,6 +2,11 @@ from sortingandsearching.sliding_window_maximum import *
 
 
 class TestSlidingWindowMaximum:
+    def test_array_length_of_2_or_less(self):
+        assert solve(k=1, arr=[1]) == [1]
+        assert solve(k=1, arr=[-1, -3]) == [-1, -3]
+        assert solve(k=2, arr=[-1, -3]) == [-1]
+
     def test_case_1(self):
         assert solve(k=2, arr=[2, 1, 1, 2]) == [2, 1, 2]
 
