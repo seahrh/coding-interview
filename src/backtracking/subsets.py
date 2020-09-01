326bcbace5b4b594bb7dd885c278f8b62d5b66c6
Ruhong <seahrh@gmail.com>
2020-09-01 20:35:50 +0800

    refactor subsets backtracking

--- src/backtracking/subsets.py
+++ src/backtracking/subsets.py
@@ -3,23 +3,29 @@ Implement an efficient algorithm for listing all k-element subsets of n items.
 Input array does not contain duplicates.
 
 SOLUTION
-Time O(N! / K!)
+Time O(2^N)
+Space O(N): depth of recursive call stack
 """
 from typing import Set, List, FrozenSet
 
 
-def _subsets(arr: List[int], k: int, partial: FrozenSet[int]) -> Set[FrozenSet[int]]:
-    if len(partial) == k:  # base case
-        return {partial}
-    res: Set[FrozenSet[int]] = set()
-    for a in arr:  # DFS
-        if a in partial:  # pruning: do not extend the partial solution
-            continue
-        res = res | _subsets(arr, k, partial | {a})
-    return res
+def _solve(
+    arr: List[int], k: int, index: int, partial: Set[int], result: Set[FrozenSet[int]]
+) -> None:
+    if len(partial) == k:  # complete solution
+        result.add(frozenset(partial))
+        return
+    if index == len(arr):  # terminate recursion
+        return
+    partial.add(arr[index])
+    _solve(arr, k, index=index + 1, partial=partial, result=result)
+    partial.remove(arr[index])
+    _solve(arr, k, index=index + 1, partial=partial, result=result)
 
 
-def subsets(arr: List[int], k: int) -> Set[FrozenSet[int]]:
+def solve(arr: List[int], k: int) -> Set[FrozenSet[int]]:
     if k > len(arr):
         raise ValueError("k must not exceed array length")
-    return _subsets(arr, k, partial=frozenset())
+    res: Set[FrozenSet[int]] = set()
+    _solve(arr, k, index=0, partial=set(), result=res)
+    return res
--- src/backtracking/tests/test_subsets.py
+++ src/backtracking/tests/test_subsets.py
@@ -3,15 +3,15 @@ from backtracking.subsets import *
 
 class TestSubsets:
     def test_subsets(self):
-        assert subsets(arr=[1], k=1) == {frozenset([1])}
-        assert subsets(arr=[1, 2, 3, 4], k=4) == {frozenset([1, 2, 3, 4])}
-        assert subsets(arr=[1, 2, 3, 4], k=1) == {
+        assert solve(arr=[1], k=1) == {frozenset([1])}
+        assert solve(arr=[1, 2, 3, 4], k=4) == {frozenset([1, 2, 3, 4])}
+        assert solve(arr=[1, 2, 3, 4], k=1) == {
             frozenset([1]),
             frozenset([2]),
             frozenset([3]),
             frozenset([4]),
         }
-        assert subsets(arr=[1, 2, 3, 4], k=2) == {
+        assert solve(arr=[1, 2, 3, 4], k=2) == {
             frozenset([1, 2]),
             frozenset([1, 3]),
             frozenset([1, 4]),
@@ -19,7 +19,7 @@ class TestSubsets:
             frozenset([2, 4]),
             frozenset([3, 4]),
         }
-        assert subsets(arr=[1, 2, 3, 4], k=3) == {
+        assert solve(arr=[1, 2, 3, 4], k=3) == {
             frozenset([1, 2, 3]),
             frozenset([1, 3, 4]),
             frozenset([1, 4, 2]),
