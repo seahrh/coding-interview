f643b60248e06fe0961ac762a734906a38485911
ruhong.seah <ruhong.seah@shopee.com>
2023-12-04 14:14:54 +0800

    search rotated sorted array

---
+++ src/sortingandsearching/lc1_search_in_rotated_sorted_array.py
@@ -0,0 +1,77 @@
+"""
+33. Search in Rotated Sorted Array https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k
+(1 <= k < nums.length) such that the resulting array is
+[nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target,
+return the index of target if it is in nums, or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+Input: nums = [1], target = 0
+Output: -1
+Constraints:
+1 <= nums.length <= 5000
+-10^4 <= nums[i] <= 10^4
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-10^4 <= target <= 10^4
+
+SOLUTION
+Modified version of binary search: one half of the array must be already sorted.
+Therefore check the sorted half to decide whether to search the left or right half.
+Time O(lg N)
+Space O(1)
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid] == target:
+                return mid
+            # left partition is sorted
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                    continue
+                lo = mid + 1
+                continue
+            # right partition is sorted
+            if nums[mid] < target <= nums[hi]:
+                lo = mid + 1
+                continue
+            hi = mid - 1
+        return -1
+
+
+# Find index_of_min element in a sorted but rotated array.
+
+
+def index_of_min(arr: List[int]) -> int:
+    """Look for min in the unsorted partition.
+    This takes O(lg n) time and O(1) space.
+    """
+    if len(arr) == 0:
+        raise ValueError("array must not be empty")
+    lo = 0
+    hi = len(arr) - 1
+    while arr[lo] > arr[hi]:
+        mid = int(lo / 2 + hi / 2)
+        if arr[lo] <= arr[mid]:  # right partition is unsorted
+            lo = mid + 1  # offset required to avoid infinite loop
+        else:  # left partition is unsorted
+            hi = mid
+    return lo
