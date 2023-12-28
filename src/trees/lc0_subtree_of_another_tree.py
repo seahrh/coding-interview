3fb8e682e8a9ce3a8e322da2fe50b9dcfeb7a4a1
Ruhong <seahrh@gmail.com>
2023-12-28 09:51:40 +0800

    subtree of another tree

---
+++ src/trees/lc0_subtree_of_another_tree.py
@@ -0,0 +1,52 @@
+"""
+572. Subtree of Another Tree https://leetcode.com/problems/subtree-of-another-tree/description/
+
+Given the roots of two binary trees root and subRoot,
+return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants.
+The tree tree could also be considered as a subtree of itself.
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+Constraints:
+The number of nodes in the root tree is in the range [1, 2000].
+The number of nodes in the subRoot tree is in the range [1, 1000].
+-10^4 <= root.val <= 10^4
+-10^4 <= subRoot.val <= 10^4
+
+SOLUTION
+Time O(MN)
+Space O(1)
+References
+- https://leetcode.com/problems/subtree-of-another-tree/solutions/4072289/using-same-tree-problem-logic-to-solve-subtree-problem-logic/
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if self.isSameTree(root, subRoot):
+            return True
+        if root is None:
+            return False
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
