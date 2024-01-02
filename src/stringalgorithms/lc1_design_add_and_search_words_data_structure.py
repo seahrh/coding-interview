e2986966f060f12096bd206ef897631408841097
Ruhong <seahrh@gmail.com>
2024-01-02 15:21:44 +0800

    design add and search words data structure

---
+++ src/stringalgorithms/lc1_design_add_and_search_words_data_structure.py
@@ -0,0 +1,66 @@
+"""
+211. Design Add and Search Words Data Structure https://leetcode.com/problems/design-add-and-search-words-data-structure/description/
+
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+Implement the WordDictionary class:
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise.
+word may contain dots '.' where dots can be matched with any letter.
+Example:
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+Constraints:
+1 <= word.length <= 25
+word in addWord consists of lowercase English letters.
+word in search consist of '.' or lowercase English letters.
+There will be at most 2 dots in word for search queries.
+At most 10^4 calls will be made to addWord and search.
+"""
+from typing import Dict
+
+
+class TrieNode:
+    def __init__(self):
+        self.mp: Dict[str, "TrieNode"] = {}
+        self.end: bool = False
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.mp:
+                curr.mp[c] = TrieNode()
+            curr = curr.mp[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        def rec(node, word: str, i: int) -> bool:
+            if i == len(word):
+                return node.end
+            if word[i] == ".":
+                for nex in node.mp.values():
+                    if rec(nex, word, i + 1):
+                        return True
+                return False
+            if word[i] not in node.mp:
+                return False
+            return rec(node.mp[word[i]], word, i + 1)
+
+        return rec(self.root, word, i=0)
