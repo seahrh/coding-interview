b289b7e1efe2a674e8eb815490359949d1e36e6e
Ruhong <seahrh@gmail.com>
2024-01-04 13:37:38 +0800

    balanced binary tree

---
+++ src/trees/lc0_balanced_binary_tree.py
@@ -0,0 +1,45 @@
+"""
+110. Balanced Binary Tree https://leetcode.com/problems/balanced-binary-tree/description/
+
+Given a binary tree, determine if it is height-balanced:
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+Input: root = []
+Output: true
+Constraints:
+The number of nodes in the tree is in the range [0, 5000].
+-10^4 <= Node.val <= 10^4
+
+SOLUTION
+References
+- https://leetcode.com/problems/balanced-binary-tree/solutions/2428871/very-easy-100-fully-explained-c-java-python-javascript-python3/
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def height(root: Optional[TreeNode]) -> int:
+            if root is None:
+                return 0
+            lef, rig = height(root.left), height(root.right)
+            # check whether subtree is balanced!
+            if lef < 0 or rig < 0 or abs(lef - rig) > 1:
+                return -1
+            return 1 + max(lef, rig)
+
+        return height(root) >= 0
