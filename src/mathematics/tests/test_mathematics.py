e8d122d8eacf4fbd83420c214597d3687f2d9699
Ruhong <seahrh@gmail.com>
2024-01-16 10:48:51 +0800

    find all numbers disappeared in an array

--- src/mathematics/tests/test_mathematics.py
+++ src/mathematics/tests/test_mathematics.py
@@ -1,16 +1,4 @@
-from mathematics import ndiegame, two_missing_numbers
-
-
-class TestTwoMissingNumbers:
-    def test_missing_numbers_at_the_ends(self):
-        assert two_missing_numbers.solve(10, [9, 8, 7, 6, 4, 5, 3, 2]) == (1, 10)
-        assert two_missing_numbers.solve(10, [8, 7, 6, 4, 5, 3, 2, 1]) == (9, 10)
-        assert two_missing_numbers.solve(10, [8, 7, 6, 4, 5, 3, 9, 10]) == (1, 2)
-
-    def test_missing_numbers_in_the_middle(self):
-        assert two_missing_numbers.solve(4, [4, 1]) == (2, 3)
-        assert two_missing_numbers.solve(10, [10, 9, 8, 7, 6, 5, 3, 1]) == (2, 4)
-        assert two_missing_numbers.solve(10, [10, 7, 6, 4, 5, 3, 2, 1]) == (8, 9)
+from mathematics import ndiegame
 
 
 class TestNDieGame:
---
+++ src/sortingandsearching/lc0_find_all_numbers_disappeared_in_an_array.py
@@ -0,0 +1,38 @@
+"""
+448. Find All Numbers Disappeared in an Array https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+
+Given an array nums of n integers where nums[i] is in the range [1, n],
+return an array of all the integers in the range [1, n] that do not appear in nums.
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+Constraints:
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+SOLUTION
+Cyclic sort: put number i in the correct index i-1 (since nums[i] is in the range [1, n])
+Time O(N)
+Space O(1)
+"""
+from typing import List
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = []
+        for i in range(n):
+            j = nums[i]
+            while nums[j - 1] != j:
+                nums[i], nums[j - 1] = nums[j - 1], nums[i]
+                j = nums[i]
+        for i in range(n):
+            if nums[i] != i + 1:
+                res.append(i + 1)
+        return res
