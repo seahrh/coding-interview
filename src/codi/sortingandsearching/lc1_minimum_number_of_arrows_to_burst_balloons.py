bf0d5549dfdbe472bfbc046a8fcc25f346703bbf
Ruhong <seahrh@gmail.com>
2024-01-19 09:11:53 +0800

    minimum number of arrows to burst balloons

---
+++ src/codi/sortingandsearching/lc1_minimum_number_of_arrows_to_burst_balloons.py
@@ -0,0 +1,60 @@
+"""
+452. Minimum Number of Arrows to Burst Balloons https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
+
+There are some spherical balloons taped onto a flat wall that represents the XY-plane.
+The balloons are represented as a 2D integer array points
+where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend.
+You do not know the exact y-coordinates of the balloons.
+Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis.
+A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend.
+There is no limit to the number of arrows that can be shot.
+A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+Example 1:
+Input: points = [[10,16],[2,8],[1,6],[7,12]]
+Output: 2
+Explanation: The balloons can be burst by 2 arrows:
+- Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+- Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+Example 2:
+Input: points = [[1,2],[3,4],[5,6],[7,8]]
+Output: 4
+Explanation: One arrow needs to be shot for each balloon for a total of 4 arrows.
+Example 3:
+Input: points = [[1,2],[2,3],[3,4],[4,5]]
+Output: 2
+Explanation: The balloons can be burst by 2 arrows:
+- Shoot an arrow at x = 2, bursting the balloons [1,2] and [2,3].
+- Shoot an arrow at x = 4, bursting the balloons [3,4] and [4,5].
+Constraints:
+1 <= points.length <= 105
+points[i].length == 2
+-231 <= xstart < xend <= 231 - 1
+
+SOLUTION
+Sort balloons in increasing order of the start position.
+Scan the sorted pairs, and maintain a pointer for the minimum end position for current "active balloons",
+whose diameters are overlapping.
+When the next balloon starts after all active balloons, shoot an arrow to burst all active balloons,
+and start to record next active balloons.
+References
+- https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/solutions/93735/a-concise-template-for-overlapping-interval-problem/
+"""
+import sys
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        res = 0
+        end = sys.maxsize
+        points.sort()
+        for p in points:
+            if p[0] > end:  # burst all active balloons
+                res += 1
+                end = p[1]
+                continue
+            end = min(end, p[1])
+        if len(points) != 0:  # last set of active balloons!
+            res += 1
+        return res
