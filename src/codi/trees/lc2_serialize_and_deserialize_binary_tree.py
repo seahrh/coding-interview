dd8e6e014a841d65f9998d973990bbd08df6e8f0
Ruhong <ruhong.seah@dyson.com>
2025-11-07 14:25:57 +0800

    refactor serialize and deserialize binary tree

--- src/codi/trees/lc2_serialize_and_deserialize_binary_tree.py
+++ src/codi/trees/lc2_serialize_and_deserialize_binary_tree.py
@@ -21,24 +21,48 @@ The number of nodes in the tree is in the range [0, 104].
 -1000 <= Node.val <= 1000
 
 SOLUTION
-DFS preorder traversal
+Serialization (Tree → String)
+Idea: Use pre-order traversal (node, left, right).
+For every node:
+Add its value to a list.
+For None children, add a special marker (here, #).
+Flatten the list into a space-separated string.
+
+Deserialization (String → Tree)
+Idea: Reverse process.
+Read the string, split by spaces, and walk through the values:
+For every value:
+If it’s #, return None.
+Otherwise, create a new node.
+Recursively build its left and right children.
+
+Why Pre-order and Mark Nulls?
+Reconstructing the exact tree shape requires knowing where the None nodes are.
+Pre-order ensures you always know at each step whether a node exists and can reliably build both children.
+Adding null markers makes your algorithm robust even for sparse or unbalanced trees.
+
+Complexity (N = number of nodes)
+Serialize: Preorder DFS, Time O(N), Space O(N), node values and mark # as null
+Deserialize: Preorder DFS, Time O(N), Space O(N), read values in order and rebuild
+Both time and space are linear, as we process each node once and store all values including Nulls.
+
 References
 - https://leetcode.com/problems/serialize-and-deserialize-binary-tree/solutions/74259/recursive-preorder-python-and-c-o-n/
 """
 
-from typing import Optional
+from typing import List, Optional
 
 from codi.trees import TreeNode
 
 
 class Codec:
     def serialize(self, root: Optional[TreeNode]) -> str:
-        vals = []
+        """Encodes a tree to a single string using pre-order traversal."""
+        vals: List[str] = []
 
-        def dfs(node: Optional[TreeNode]) -> None:
-            nonlocal vals  # noqa: F824
-            if node is None:
-                vals.append("#")
+        def dfs(node):
+            if not node:
+                vals.append("#")  # Use '#' as a null marker
                 return
             vals.append(str(node.val))
             dfs(node.left)
@@ -48,9 +72,10 @@ class Codec:
         return " ".join(vals)
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree."""
         vals = iter(data.split())
 
-        def dfs() -> Optional[TreeNode]:
+        def dfs():
             val = next(vals)
             if val == "#":
                 return None
